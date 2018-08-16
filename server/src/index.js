027fdecc74cec5bfe34ded37195ea2efb9562f1c
Evan Nichols <evantnichols@gmail.com>
2018-08-16 00:53:52 -0400

    progress but messy

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,7 @@ const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-
-const SECRET = 'asecret'
+const { Context, getUserId, APP_SECRET } = require('./utils')
 
 const resolvers = {
   Query: {
@@ -14,16 +13,16 @@ const resolvers = {
       return ctx.db.query.posts({ where: { isPublished: false } }, info)
     },
     post(parent, { id }, ctx, info) {
-      return ctx.db.query.post({ where: { id } }, info)
+      return ctx.db.query.post({ where: { id: id } }, info)
     },
     user(parent, { id }, ctx, info) {
-      return ctx.db.query.user({ where: { id } }, info)
+      return ctx.db.query.user({ where: { id: id } }, info)
     },
     users(parent, args, ctx, info) {
       return ctx.db.query.users()
     },
     bookshelf(parent, { id }, ctx, info) {
-      return ctx.db.query.bookshelfs({ where: { id } }, info)
+      return ctx.db.query.bookshelves({ where: { id: id } }, info)
     }
   },
   Mutation: {
@@ -39,12 +38,12 @@ const resolvers = {
       )
     },
     deletePost(parent, { id }, ctx, info) {
-      return ctx.db.mutation.deletePost({ where: { id } }, info)
+      return ctx.db.mutation.deletePost({ where: { id: id } }, info)
     },
     publish(parent, { id }, ctx, info) {
       return ctx.db.mutation.updatePost(
         {
-          where: { id },
+          where: { id: id },
           data: { isPublished: true },
         },
         info,
@@ -52,7 +51,6 @@ const resolvers = {
     },
     async createUser(parent, { firstName, lastName, email, password }, ctx, info) {
       const hashedPassword = await bcrypt.hash(password, 10)
-
       const user = await ctx.db.mutation.createUser({
         data: {
           firstName: firstName,
@@ -62,6 +60,13 @@ const resolvers = {
         }
       })
 
+      // TEST: Just seeding everyone's library with a single book.
+      const seedbooks = await ctx.db.query.books()
+
+      console.log('!!!' + seedbooks)
+
+      // const seedConnect = seedbooks
+
       const bookshelf = await ctx.db.mutation.createBookshelf({
         data: {
           owner: {
@@ -69,12 +74,14 @@ const resolvers = {
               id: user.id
             }
           },
-          books: null
+          books: {
+            connect: null
+          }
         }
       })
 
       return {
-        token: jwt.sign({ userId: user.id }, SECRET),
+        token: jwt.sign({ userId: user.id }, APP_SECRET),
         user: user,
         bookshelf: bookshelf
       }
@@ -83,6 +90,7 @@ const resolvers = {
       return ctx.db.mutation.deleteUser({ where: { id: id } }, info)
     },
     async login(parent, { email, password }, ctx, info) {
+
       const user = await ctx.db.query.user({ where: { email: email } } );
       if(!user){
         throw new Error(`No user found for email: ${email}`)
@@ -93,9 +101,16 @@ const resolvers = {
         throw new Error('Invalid password')
       }
 
+      const bookshelf = await ctx.db.query.bookshelf({ where: { owner: user }})
+
+      if(!bookshelf){
+        throw new Error('No bookshelf for user')
+      }
+
       return {
-        token: jwt.sign({ userId: user.id }, SECRET),
-        user
+        token: jwt.sign({ userId: user.id }, APP_SECRET),
+        user: user,
+        bookshelf: bookshelf
       }
     },
   },
@@ -110,7 +125,6 @@ const server = new GraphQLServer({
       typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
       endpoint: 'https://eu1.prisma.sh/public-junglepig-932/digital-bookshelf/dev', // the endpoint of the Prisma API
       debug: true, // log all GraphQL queries & mutations sent to the Prisma API
-      // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
     }),
   }),
 })
