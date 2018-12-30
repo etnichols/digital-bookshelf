b0a950323fceba9a4d86fd0b3fc66f54bcccb68b
Evan Nichols <evantnichols@gmail.com>
2018-12-29 18:32:47 -0600

    Add boilerplate StyledText component

---
+++ mobile/components/StyledText.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+
+// Custom color?
+
+// Text Styles Enum.
+const TextStyles = Object.freeze({
+  BODY: 1,
+  DETAIL: 2,
+  HEADER_1: 3,
+  HEADER_2: 4,
+  HEADER_3: 5,
+  SUBTITLE: 6,
+})
+
+export default class Launch extends React.Component {
+  render(){
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+})
